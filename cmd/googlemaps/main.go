c1772fa9af9c880f748ccfc3a4a2e272c05830ab
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-10-29 05:37:40 +0000

    Added googlemaps example.

---
+++ cmd/googlemaps/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Google Maps embed API demo")
+
+	// root handler
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, path is: %q\n", html.EscapeString(r.URL.Path))
+	})
+
+	// static content handler
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	// start serving
+	log.Fatal(http.ListenAndServeTLS(":8080", "/h/certbot/rasp.beyondbroadcast.com/fullchain.pem",
+		"/h/certbot/rasp.beyondbroadcast.com/privkey.pem", nil))
+}
