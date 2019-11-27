784a9f306a7176f96b899648aaac34cd463782b8
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-11-27 00:53:19 +0000

    Added nkeys (NATS keys) example.

---
+++ cmd/nats/nkeys/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/nats-io/nkeys"
+	"github.com/nats-io/nuid"
+)
+
+func main() {
+	fmt.Println("NATS keys")
+	//user, err := nkeys.CreateUser()
+	user, err := nkeys.CreateAccount()
+	if err != nil {
+		log.Fatal("1 ", err)
+	}
+
+	challenge := nuid.Next()
+	fmt.Println(challenge)
+
+	signature, _ := user.Sign([]byte(challenge))
+
+	seed, err := user.Seed()
+	if err != nil {
+		log.Fatal("2 ", err)
+	}
+	fmt.Printf("seed: %s\n", seed)
+
+	usr2, _ := nkeys.FromSeed(seed)
+	seed2, _ := usr2.Seed()
+	if string(seed2) != string(seed) {
+		log.Fatalf("seed compare failed")
+	}
+
+	pubKey, _ := user.PublicKey()
+	usr3, _ := nkeys.FromPublicKey(pubKey)
+	if err := usr3.Verify([]byte(challenge), signature); err != nil {
+		log.Printf("challenge failed: %v")
+	}
+
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.13
 require (
 	contrib.go.opencensus.io/exporter/ocagent v0.6.0
 	github.com/coreos/go-oidc v2.1.0+incompatible
+	github.com/nats-io/nkeys v0.0.2
+	github.com/nats-io/nuid v1.0.1
 	github.com/nats-io/stan.go v0.5.0
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
 	github.com/siuyin/dflt v0.0.0-20190616123008-ea16caf9b8ef
