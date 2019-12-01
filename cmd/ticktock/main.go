7f7d91ea45a16150a3da6f1961aa204d162c436b
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-12-01 14:51:00 +0000

    Added env example to kustomize deploy.

--- cmd/ticktock/main.go
+++ cmd/ticktock/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/siuyin/dflt"
 )
 
 func main() {
+	stage := dflt.EnvString("STAGE", "dev")
 	fmt.Println("ticktock prints the time once a second")
 	for {
-		fmt.Println(time.Now().Format("time: 2006-01-02 15:04:05.00 MST"))
+		fmt.Printf("%s: %s\n", stage, time.Now().Format("2006-01-02 15:04:05.00 MST"))
 		time.Sleep(time.Second)
 	}
 }
