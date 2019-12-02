4cf11728383ce8311ba7bdf3731463852a31bf33
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-12-02 11:27:27 +0000

    Updated ticktock to use configmaps.

--- cmd/ticktock/main.go
+++ cmd/ticktock/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	stage := dflt.EnvString("STAGE", "dev")
 	msg := dflt.EnvString("MSG", "dev-msg")
-	fmt.Println("ticktock prints the time once a second")
+	fmt.Printf("%s ticktock prints the time once a second", stage)
 	for {
 		fmt.Printf("%s: %s: %s\n", stage, msg, time.Now().Format("2006-01-02 15:04:05.00 MST"))
 		time.Sleep(time.Second)
