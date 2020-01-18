400b29f50002cc2e776540dc6dc9a737e7437936
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-01-18 07:14:17 +0000

    Formatted ticktock main.

--- cmd/ticktock/main.go
+++ cmd/ticktock/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	stage := dflt.EnvString("STAGE", "dev")
 	msg := dflt.EnvString("MSG", "dev-msg")
-	fmt.Printf("%s ticktock prints the time once a second", stage)
+	fmt.Printf("%s ticktock prints the time once a second\n", stage)
 	for {
 		fmt.Printf("%s: %s: %s\n", stage, msg, time.Now().Format("2006-01-02 15:04:05.00 MST"))
 		time.Sleep(time.Second)
