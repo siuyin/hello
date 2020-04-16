2b9e01cf8b07e4b671482425f67497532540c2db
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-04-16 09:16:06 +0000

    Added dummy tests for helloworld.

--- cmd/helloworld/main_test.go
+++ cmd/helloworld/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"testing"
 
 	"github.com/siuyin/hello/world"
 )
@@ -10,4 +11,16 @@ func ExampleHello() {
 	fmt.Println(world.Greet())
 	// Output:
 	// Hello
+}
+
+func TestAdd(t *testing.T) {
+	if 1+1 != 2 {
+		t.Error("failed test")
+	}
+}
+
+func TestSub(t *testing.T) {
+	if 3-1 != 2 {
+		t.Error("failed test")
+	}
 }
