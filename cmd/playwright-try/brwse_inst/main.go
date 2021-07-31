7f17da1362cd5dc97edbb2df7dfc87cd058f4428
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-07-31 12:43:50 +0800

    feat: add browser install example

--- cmd/playwright-try/brwse_inst/main.go
+++ cmd/playwright-try/brwse_inst/main.go
@@ -8,19 +8,38 @@ import (
 )
 
 func main() {
-	pw, err := playwright.Run()
+	pw := initPlaywright()
+	defer clsPlaywright(pw)
+
+	brwse, pg := browserPage(pw)
+	defer clsBrowser(brwse)
+
+	scrape(pg)
+}
+
+func initPlaywright() *playwright.Playwright {
+	pw, err := playwright.Run(
+		&playwright.RunOptions{
+			DriverDirectory: "/home/siuyin/go",
+			Browsers:        []string{"chromium"}},
+	)
 	if err != nil {
 		log.Fatalf("could not start playwright: %v", err)
 	}
-	hl := true
+	return pw
+}
+
+func browserPage(pw *playwright.Playwright) (playwright.Browser, playwright.Page) {
 	opt := playwright.BrowserTypeLaunchOptions{
-		Headless: &hl,
+		Headless: playwright.Bool(true),
 		Args:     []string{"--disable-gpu"},
 	}
 	browser, err := pw.Chromium.Launch(opt)
 	if err != nil {
 		log.Fatalf("could not launch browser: %v", err)
 	}
+	fmt.Printf("Browser version: %s %s\n", pw.Chromium.Name(), browser.Version())
+
 	page, err := browser.NewPage()
 	if err != nil {
 		log.Fatalf("could not create page: %v", err)
@@ -28,7 +47,11 @@ func main() {
 	if _, err = page.Goto("https://news.ycombinator.com"); err != nil {
 		log.Fatalf("could not goto: %v", err)
 	}
-	entries, err := page.QuerySelectorAll(".athing")
+	return browser, page
+}
+
+func scrape(pg playwright.Page) {
+	entries, err := pg.QuerySelectorAll(".athing")
 	if err != nil {
 		log.Fatalf("could not get entries: %v", err)
 	}
@@ -43,10 +66,16 @@ func main() {
 		}
 		fmt.Printf("%d: %s\n", i+1, title)
 	}
-	if err = browser.Close(); err != nil {
+}
+
+func clsBrowser(browser playwright.Browser) {
+	if err := browser.Close(); err != nil {
 		log.Fatalf("could not close browser: %v", err)
 	}
-	if err = pw.Stop(); err != nil {
+}
+
+func clsPlaywright(pw *playwright.Playwright) {
+	if err := pw.Stop(); err != nil {
 		log.Fatalf("could not stop Playwright: %v", err)
 	}
 }
