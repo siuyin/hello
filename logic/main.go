37880927b13e20832371c93c4f53dcd485115efc
Loh Siu Yin <siuyin@beyondbroadcast.com>
2022-11-03 01:51:32 +0000

    Added comments.

--- logic/main.go
+++ logic/main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("propositional logic examples")
-	// logic symbols representing propositions which are either true or false.
+	// Logic symbols representing propositions which are either true or false.
+	// Below is one example or "model" of the logic problem.
 	var aa, ab, ac, ad, ba, bb, bc, bd, ca, cb, cc, cd, da, db, dc, dd bool
 	aa = true
 	bd = true
@@ -12,14 +13,15 @@ func main() {
 	dc = true
 	println(aa, ab, ac, ad, ba, bb, bc, bd, ca, cb, cc, cd, da, db, dc, dd)
 
-	kb := !bc &&
+	knowledge := !bc &&
 		(!bb || dd) &&
 		(!ac || da) &&
 		(dd || !ba) &&
 		(!ca || bb) &&
 		((bb || cc) || dc)
 
-	if kb &&
+	// if knowlege is true when evaluated against a model AND our query also evaluates to true, then our query entails the knowledge.
+	if knowledge &&
 		(aa && bd && cb && dc) {
 		println("verified")
 	}
