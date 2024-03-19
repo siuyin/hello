687263f8544df343dd4dff02628cdf38a3fbe917
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-03-19 21:38:42 +0800

    add: styles.css to embedded web example

--- cmd/embeddedweb/main.go
+++ cmd/embeddedweb/main.go
@@ -6,11 +6,10 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/siuyin/hello/cmd/embeddedweb/public"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.FS(public.Content)))
+	//http.Handle("/", http.FileServer(http.FS(public.Content)))
+	http.Handle("/", http.FileServer(http.Dir("public")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
