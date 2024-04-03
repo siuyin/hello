eefb6101621c06ebf4e865519ac8439918826280
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-04-03 13:30:34 +0800

    feat: make a 2 batch of images

--- cmd/google-ocr/main.go
+++ cmd/google-ocr/main.go
@@ -22,24 +22,20 @@ func main() {
 	defer client.Close()
 
 	// Sets the name of the image file to annotate.
-	filename := "/home/siuyin/Downloads/old/increaseAircon IMG_20240403_112530~2.jpg"
-
-	// file, err := os.Open(filename)
-	// if err != nil {
-	// 	log.Fatalf("Failed to read file: %v", err)
-	// }
-	// defer file.Close()
-
-	imgBytes, err := os.ReadFile(filename)
-	if err != nil {
-		log.Fatalf("Failed to read file: %v", err)
-	}
+	fn1 := "/home/siuyin/Downloads/old/increaseAircon IMG_20240403_112530~2.jpg"
+	fn2 := "/home/siuyin/Downloads/old/brownfox IMG_20240403_111925~2.jpg"
 
 	req := &visionpb.BatchAnnotateImagesRequest{
-		Requests: []*visionpb.AnnotateImageRequest{{
-			Image: &visionpb.Image{Content: imgBytes},
-			Features: []*visionpb.Feature{{Type: visionpb.Feature_DOCUMENT_TEXT_DETECTION}},
-		}},
+		Requests: []*visionpb.AnnotateImageRequest{
+			{
+				Image:    &visionpb.Image{Content: imgContent(fn1)},
+				Features: []*visionpb.Feature{{Type: visionpb.Feature_DOCUMENT_TEXT_DETECTION}},
+			},
+			{
+				Image:    &visionpb.Image{Content: imgContent(fn2)},
+				Features: []*visionpb.Feature{{Type: visionpb.Feature_DOCUMENT_TEXT_DETECTION}},
+			},
+		},
 	}
 
 	resp, err := client.BatchAnnotateImages(ctx, req)
@@ -48,6 +44,14 @@ func main() {
 	}
 
 	for _, r := range resp.Responses {
-		fmt.Printf(r.FullTextAnnotation.Text)
+		fmt.Printf("----\n%s\n",r.FullTextAnnotation.Text)
 	}
+}
+
+func imgContent(filename string) []byte {
+	imgBytes, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+	return imgBytes
 }
