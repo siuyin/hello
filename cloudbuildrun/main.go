41af7c2ddb71ffb45833fa0a5a50fe90ba195c5a
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-12-18 12:28:20 +0800

    update main

--- cloudbuildrun/main.go
+++ cloudbuildrun/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello world!")
+		io.WriteString(w, "Hello world")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
